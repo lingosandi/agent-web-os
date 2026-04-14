824eeb8990c5555174dae6ef0418e1746327e2e6
lingosandi <91113613+lingosandi@users.noreply.github.com>
2026-04-14 21:18:34 +0800

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config"
 import path from "path"
 
 export default defineConfig({
+    base:'/agent-web-os',
     test: {
         include: ["src/**/*.test.ts"],
     },
