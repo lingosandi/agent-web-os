d6d2eec566d14b9c9c753d6426ab4a2f43344b87
lingosandi <91113613+lingosandi@users.noreply.github.com>
2026-04-14 21:30:39 +0800

    Update vite.config.ts

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
-const pagesBase = process.env.GITHUB_ACTIONS && repoName ? `/${repoName}/` : '/'
+// const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+// const pagesBase = process.env.GITHUB_ACTIONS && repoName ? `/${repoName}/` : '/'
 
 // https://vite.dev/config/
 export default defineConfig({
   // base: pagesBase,
-  base:'/agent-web-os/'
+  base:'/agent-web-os/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
